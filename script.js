f20283f060aaa66d445f0b884cb82e861e3c526e
Yukako Sunabe <yukakosunabe@gmail.com>
2018-11-08 16:10:35 -0800

    fixed side nav bar and h1 for about me page

--- script.js
+++ script.js
@@ -1,72 +1,96 @@
-var TxtType = function (el, toRotate, period) {
-    this.toRotate = toRotate;
-    this.el = el;
-    this.loopNum = 0;
-    this.period = parseInt(period, 10) || 2000;
-    this.txt = '';
-    this.tick();
+var TxtType = function(el, toRotate, period) {
+  this.toRotate = toRotate;
+  this.el = el;
+  this.loopNum = 0;
+  this.period = parseInt(period, 10) || 2000;
+  this.txt = "";
+  this.tick();
+  this.isDeleting = false;
+};
+
+TxtType.prototype.tick = function() {
+  var i = this.loopNum % this.toRotate.length;
+  var fullTxt = this.toRotate[i];
+
+  if (this.isDeleting) {
+    this.txt = fullTxt.substring(0, this.txt.length - 1);
+  } else {
+    this.txt = fullTxt.substring(0, this.txt.length + 1);
+  }
+
+  this.el.innerHTML = '<span class="wrap">' + this.txt + "</span>";
+
+  var that = this;
+  var delta = 200 - Math.random() * 100;
+
+  if (this.isDeleting) {
+    delta /= 2;
+  }
+
+  if (!this.isDeleting && this.txt === fullTxt) {
+    delta = this.period;
+    this.isDeleting = true;
+  } else if (this.isDeleting && this.txt === "") {
     this.isDeleting = false;
+    this.loopNum++;
+    delta = 500;
+  }
+
+  setTimeout(function() {
+    that.tick();
+  }, delta);
 };
 
-TxtType.prototype.tick = function () {
-    var i = this.loopNum % this.toRotate.length;
-    var fullTxt = this.toRotate[i];
-
-    if (this.isDeleting) {
-        this.txt = fullTxt.substring(0, this.txt.length - 1);
-    } else {
-        this.txt = fullTxt.substring(0, this.txt.length + 1);
+window.onload = function() {
+  var elements = document.getElementsByClassName("typewrite");
+  for (var i = 0; i < elements.length; i++) {
+    var toRotate = elements[i].getAttribute("data-type");
+    var period = elements[i].getAttribute("data-period");
+    if (toRotate) {
+      new TxtType(elements[i], JSON.parse(toRotate), period);
     }
-
-    this.el.innerHTML = '<span class="wrap">' + this.txt + '</span>';
-
-    var that = this;
-    var delta = 200 - Math.random() * 100;
-
-    if (this.isDeleting) {
-        delta /= 2;
-    }
-
-    if (!this.isDeleting && this.txt === fullTxt) {
-        delta = this.period;
-        this.isDeleting = true;
-    } else if (this.isDeleting && this.txt === '') {
-        this.isDeleting = false;
-        this.loopNum++;
-        delta = 500;
-    }
-
-    setTimeout(function () {
-        that.tick();
-    }, delta);
+  }
 };
-
-window.onload = function () {
-    var elements = document.getElementsByClassName('typewrite');
-    for (var i = 0; i < elements.length; i++) {
-        var toRotate = elements[i].getAttribute('data-type');
-        var period = elements[i].getAttribute('data-period');
-        if (toRotate) {
-            new TxtType(elements[i], JSON.parse(toRotate), period);
-        }
-    }
-};
-
 
 // side scrollbars
 function openNav() {
-    document.getElementById("mySidenav").style.display = "block";
-    document.getElementById("mySidenav").style.width = "100%";
-    document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
+  document.getElementById("mySidenav").style.display = "block";
+  document.getElementById("mySidenav").style.width = "100%";
+  document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
 }
 
 function closeNav() {
-    document.getElementById("mySidenav").style.width = "0";
-    document.body.style.backgroundColor = "#272725";
+  document.getElementById("mySidenav").style.width = "0";
+  document.body.style.backgroundColor = "#272725";
 }
 
+function pageChange(index) {
+  var pageIds = [
+    "#home",
+    "#about",
+    "#project",
+    "#project1",
+    "#project2",
+    "#contact"
+  ];
+  var href = document.getElementsByTagName("a")[index].getAttribute("href");
+  for (var i = 0; i < pageIds.length; i++) {
+    var li = document.getElementById("menu").children[i];
+    if (li.className == "active") {
+      li.classList.remove("active");
+    }
+    if (href == pageIds[i]) {
+      li.classList.add("active");
+    }
+  }
+}
+
+function checkId() {
+  var href = document.getElementsByTagName("a")[1].getAttribute("href");
+  console.log(href);
+}
 
 // project button
 function goToSite() {
-    document.getElementById("round").open('https://github.com/yukako098');
+  document.getElementById("round").open("https://github.com/yukako098");
 }
