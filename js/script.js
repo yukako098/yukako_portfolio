3d83281b81c41df92cab8a685fcadc4009cc5f58
Yukako Sunabe <yukakosunabe@gmail.com>
2018-12-29 15:33:33 -0800

    fixed sidenavbar's bag

--- js/script.js
+++ js/script.js
@@ -41,53 +41,56 @@ TxtType.prototype.tick = function() {
     }, delta);
 };
 
-window.onload = function() {
-    var elements = document.getElementsByClassName("typewrite");
-    for (var i = 0; i < elements.length; i++) {
-        var toRotate = elements[i].getAttribute("data-type");
-        var period = elements[i].getAttribute("data-period");
-        if (toRotate) {
-            new TxtType(elements[i], JSON.parse(toRotate), period);
-        }
-    }
-};
+// window.onload = function() {
+//     var elements = document.getElementsByClassName("typewrite");
+//     for (var i = 0; i < elements.length; i++) {
+//         var toRotate = elements[i].getAttribute("data-type");
+//         var period = elements[i].getAttribute("data-period");
+//         if (toRotate) {
+//             new TxtType(elements[i], JSON.parse(toRotate), period);
+//         }
+//     }
+// };
 
 // side scrollbars
 function openNav() {
     document.getElementById("mySidenav").style.display = "block";
     document.getElementById("mySidenav").style.width = "100%";
-    document.getElementById("projects").style.display = "none";
 }
 
 function closeNav() {
     document.getElementById("mySidenav").style.width = "0";
-    document.getElementById("projects").style.display = "block";
-    document.getElementById("projects").style.paddingTop = "5%";
 }
 
 // Initial Y animation
 
-function pageChange(index) {
-    var pageIds = [
-        "#home",
-        "#about",
-        "#project",
-        "#project1",
-        "#project2",
-        "#contact"
-    ];
-    var href = document.getElementsByTagName("a")[index].getAttribute("href");
-    for (var i = 0; i < pageIds.length; i++) {
-        var li = document.getElementsByTagName("body").children[i];
-        if (li.className.contain("fp-viewing-page")) {
-            li.classList.removeAll();
-            li.classList.add("fp-viewing-page" + i + 1);
-        }
-        // if (href == pageIds[i]) {
-        //     li.classList.add("active");
-        // }
-    }
-}
+// function pageChange(index) {
+//     var pageIds = [
+//         "fp-viewing-top",
+//         "fp-viewing-about-me",
+//         "fp-viewing-granma",
+//         "fp-viewing-getInTouch"
+//     ];
+//     //var href = document.getElementsByTagName("a")[index].getAttribute("href");
+//     //console.log(href);
+//     var bodyClass = document.getElementById("body").className;
+//     console.log(bodyClass);
+//     bodyClass = "";
+//     console.log(bodyClass);
+//     bodyClass = bodyClass.concat(pageIds[index]);
+//     console.log(bodyClass);
+
+//     // for (var i = 0; i < pageIds.length; i++) {
+//     //     var li = document.getElementsByTagName("body").children[i];
+//     //     if (li.className.contain("fp-viewing-page")) {
+//     //         li.classList.removeAll();
+//     //         li.classList.add("fp-viewing-page" + i + 1);
+//     //     }
+//     //     if (href == pageIds[i]) {
+//     //         li.classList.add("active");
+//     //     }
+//     // }
+// }
 
 function checkId() {
     var href = document.getElementsByTagName("a")[1].getAttribute("href");
